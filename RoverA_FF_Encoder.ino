c4079f637259df0d43ef54def2590b3ec08bed24
steve.rhoads@radcorp.com <steve.rhoads@radcorp.com>
2013-02-24 17:41:46 -0500

    tweaking encoder, wheel size parameters

--- RoverA_FF_Encoder.ino
+++ RoverA_FF_Encoder.ino
@@ -17,15 +17,18 @@
 const int RIGHT               = 0;
 const int LEFT                = 1;
 
-const int ENC_DIST_SLOW       = 10;   // point at which we slow down to hit encoder target exactly
+const int ENC_DIST_SLOW       = 17;   // point at which we slow down to hit encoder target exactly
 const int ENC_SPEED_SLOW      = 25;  // speed percent used to approach target encoder
 
-int motor_speed = 100;
-int pwm;
 const int LED_PIN             = 13;
 
 const int PULSE_PER_ROTATION  = 20;
-const float WHEEL_DIAMETER    = 2.6; // inches
+const float WHEEL_DIAMETER    = 2.4; // inches
+const int MIN_RUN_TIME        = 100; // milliseconds after a motor run command...to give time to act
+
+int motor_speed = 100;
+int pwm;
+
 //const float PI                = 3.1415927;
 
 // FORWARD, BACKWARD, BRAKE, RELEASE
@@ -36,7 +39,8 @@ void InitSerial(int baud_rate);
 int CalculateEncoderTarget(float inches);
 int Drive(DriveDirection drive_dir, int drive_speed, float drive_distance); 
 void Turn(TurnType turn_type, TurnDirection turn_dir, int drive_speed, float drive_distance); 
-int SetSpeed(int motor, int target_speed);
+int SetSpeed(int encoder_current_value, int encoder_target_value, int target_speed_pct);
+int SetDriveDirection(DriveDirection drive_direction, int encoder_current_value, int encoder_target_value);
 void StopMotors();
 void PrintEncoders();
 void ResetEncoders();
@@ -44,10 +48,10 @@ void ResetEncoders();
 //AF_DCMotor Motor_Left(4, MOTOR34_1KHZ);   // both left motors on port 4 
 //AF_DCMotor Motor_Left(1, MOTOR12_1KHZ);   // both left motors on port 4 
 //AF_DCMotor Motor_Left(2, MOTOR12_1KHZ);   // both left motors on port 4 
-AF_DCMotor Motor_Left(4);
+AF_DCMotor Motor_Left(4, MOTOR34_64KHZ);
 
 //AF_DCMotor Motor_Right(3, MOTOR34_1KHZ);  // both right motors on port 3
-AF_DCMotor Motor_Right(3);
+AF_DCMotor Motor_Right(3, MOTOR34_64KHZ);
 
 // on a DUE, this is the pin #
 // on a Mega2560, it is the interrupt. INT#(PIN#): 0(2),1(3),2(21),3(20),4(19),5(18)
@@ -70,7 +74,7 @@ void setup(){
   // initialize the motors to stop...(Left,Right,Speed[0..255],waitMsec)
   PrintEncoders();
   //int Drive(DriveDirection, int drive_speed, float drive_distance); 
-  Drive(DRIVE_DIR_FWD,100,10); // 100% for 10inches
+  Drive(DRIVE_DIR_FWD,100,25); // 100% for 10inches
   StopMotors();
   PrintEncoders();
 }
@@ -121,17 +125,8 @@ int Drive(DriveDirection drive_direction, int drive_speed, float drive_distance)
   // translate inches to encoder target
   int enc_target = CalculateEncoderTarget(drive_distance);
   if (drive_direction == DRIVE_DIR_REV){
-    DirectionLeft   = BACKWARD;
-    DirectionRight  = BACKWARD;
     enc_target      *= -1; // make it negative
-  }
-  else {
-    DirectionLeft   = FORWARD;
-    DirectionRight  = FORWARD;
-  }
-  // let's see what the computer thinks
-  Serial3.println("EncTarget=" + String(enc_target) + " Direction=" + String(DirectionLeft));;
-  
+  }  
   // Set basic speed, unless we're close on the encoder, then override with a slow speed.
   // Overall, we'll move until the encoders hit the desired value - using a while loop
   // Reset both encoders
@@ -140,48 +135,19 @@ int Drive(DriveDirection drive_direction, int drive_speed, float drive_distance)
   // now work the encoders until they both show the target value
   // TODO: right motors aren't working, ... so not including them in this equation.
   while ((encoder[LEFT] != enc_target)){
-    Serial3.println("ELC:" + String(encoder[LEFT]) + "/ELT:" + String(enc_target) + "-Dir:" + String(encoder[LEFT] < enc_target) + "-DL:" + DirectionLeft );  
-    
-    // Set LEFT Direction ---------------------------------------
-    if (encoder[LEFT] < enc_target){
-      DirectionLeft  = FORWARD;
-    }
-    else if (encoder[LEFT] > enc_target){
-      DirectionLeft  = BACKWARD;
-    }
-    else {
-      DirectionLeft  = BRAKE;  // may need to use RELEASE
-    }  
-    // Set RIGHT Direction ---------------------------------------
-    if (encoder[RIGHT] < enc_target){
-      DirectionRight  = FORWARD;
-    }
-    else if (encoder[RIGHT] > enc_target){
-      DirectionRight  = BACKWARD;
-    }
-    else {
-      DirectionRight  = BRAKE;  // may need to use RELEASE
-    } 
-    
-    // Set Speed
-    // if we're close, let's slow down. Our encoders are 20 PulsePerRotation (PPR)
-    // Set Left, check absolute value
-    if (abs(enc_target - encoder[LEFT]) > ENC_DIST_SLOW) {
-      SetSpeed(LEFT,drive_speed);
-    }
-    else {
-      SetSpeed(LEFT, ENC_SPEED_SLOW); // speed is in percent (0-100)
-    }
-    // Set Right, check absolute value
-    if (abs(enc_target - encoder[RIGHT]) > ENC_DIST_SLOW) {
-      SetSpeed(RIGHT,drive_speed);
-    }
-    else {
-      SetSpeed(RIGHT, ENC_SPEED_SLOW); // speed is in percent (0-100)
-    }
+    //int SetDriveDirection(DriveDirection drive_direction, int encoder_current_value, int encoder_target_value)
+    DirectionLeft   = SetDriveDirection(drive_direction, encoder[LEFT], enc_target);
+    DirectionRight  = SetDriveDirection(drive_direction, encoder[RIGHT], enc_target);  
+    // set speed. If close to target, slow down.
+    Motor_Left.setSpeed(SetSpeed(encoder[LEFT], enc_target, drive_speed));
+    Motor_Right.setSpeed(SetSpeed(encoder[RIGHT], enc_target, drive_speed));
+    // debug
+    Serial3.println("ELC:" + String(encoder[LEFT]) + "/ELT:" + String(enc_target) + "-Dir:" + String(encoder[LEFT] < enc_target) + "-DL:" + DirectionLeft + " Speed:" + SetSpeed(encoder[LEFT], enc_target, drive_speed));  
     // Now move
-    Motor_Left.run(DirectionLeft);
+    // Motor_Left.run(DirectionLeft);
+    Motor_Left.run(BACKWARD);
     Motor_Right.run(DirectionRight);
+    delay(MIN_RUN_TIME); // need a little time so that it can do what it plans    
   } // while not encMotor = encoder[Motor]  
   // now reset the encoders! You've reached your target
   ResetEncoders();  
@@ -197,26 +163,24 @@ void PrintEncoders(){
   PrintSerial(message);    
 }
 
-int SetSpeed(int motor, int target_speed){
-  int scaled_speed=0;
+int SetSpeed(int encoder_current_value, int encoder_target_value, int target_speed_pct){
+  // calculate how far to go
+  int remaining     = abs(abs(encoder_current_value)-abs(encoder_target_value));
+  // if we have more than a threshold distance to go, go with the full scaled speed, otherwise slow down.
+  if (remaining < ENC_DIST_SLOW){
+    target_speed_pct = ENC_SPEED_SLOW; // override with global "slow" setting
+  }
   // scale motor percent(0-100) into pwm range (0-255) 
-  scaled_speed  = map(target_speed, 0,100, 0,255);  
-  scaled_speed  = constrain(scaled_speed,0,255);
-
- if (motor == RIGHT){
-   Motor_Right.setSpeed(scaled_speed);    
- }
- else {
-   Motor_Left.setSpeed(scaled_speed);
- }
+  int scaled_speed  = map(target_speed_pct, 0,100, 0,255);  
+  return scaled_speed      = constrain(scaled_speed,0,255);
 }
 
 void StopMotors(){
   Motor_Left.setSpeed(0);
   Motor_Right.setSpeed(0);
   // set direction also includes FORWARD|BACKWARD|RELEASE|BRAKE
-  Motor_Left.run(BRAKE);
-  Motor_Right.run(BRAKE);
+  Motor_Left.run(RELEASE);
+  Motor_Right.run(RELEASE);
 }
 
 void ResetEncoders(){
@@ -224,62 +188,37 @@ void ResetEncoders(){
   encoder[RIGHT]  = 0;
 }
 
-void Turn(TurnType turn_dir, int target_speed, int distance_inches){
-  /*
-  // Set basic speed, unless we're close on the encoder, then override with a slow speed.
-  // Overall, we'll move until the encoders hit the desired value - use a big while loop
-  //while ((encoder[LEFT] != encLeft) && (encoder[RIGHT] != encRight)){
-    while ((encoder[LEFT] != encLeft)){
-        Serial3.println("ELC:" + String(encoder[LEFT]) + "/ELT:" + String(encLeft) + "-Dir:" + String(encoder[LEFT]<encLeft) + "-DL:" + DirectionLeft );  
-    //PrintEncoders();
-    // Set the direction *****************************************************
-    //DirectionLeft   = directionLeft;
-    //DirectionRight  = directionRight;
-    
-    // Set LEFT Direction ---------------------------------------
-    if (encoder[LEFT] < encLeft){
-      DirectionLeft  = FORWARD;
-    }
-    else if (encoder[LEFT] > encLeft){
-      DirectionLeft  = BACKWARD;
-    }
-    else {
-      DirectionLeft  = BRAKE;  // may need to use RELEASE
-    }  
-    // Set RIGHT Direction ---------------------------------------
-    if (encoder[RIGHT] < encRight){
-      DirectionRight  = FORWARD;
-    }
-    else if (encoder[RIGHT] > encRight){
-      DirectionRight  = BACKWARD;
-    }
-    else {
-      DirectionRight  = BRAKE;  // may need to use RELEASE
-    } 
-    
-    // Set Speed
-    // if we're close, let's slow down. Our encoders are 20 PulsePerRotation (PPR)
-    // Set Left, check absolute value
-    if (abs(encLeft - encoder[LEFT]) > ENC_DIST_SLOW) {
-      SetSpeed(LEFT,speedLeft);
-    }
-    else {
-      SetSpeed(LEFT, ENC_SPEED_SLOW); // speed is in percent (0-100)
-    }
-    // Set Right, check absolute value
-    if (abs(encRight - encoder[RIGHT]) > ENC_DIST_SLOW) {
-      SetSpeed(RIGHT,speedRight);
-    }
-    else {
-      SetSpeed(RIGHT, ENC_SPEED_SLOW); // speed is in percent (0-100)
-    }
-    // Now move
-    Motor_Left.run(DirectionLeft);
-    Motor_Right.run(DirectionRight);
-  } // while not encMotor = encoder[Motor]  
-  // now reset the encoders! You've reached your target
-  ResetEncoders();  
-  */
+void Turn(TurnType turn_type,TurnDirection turn_dir, int target_speed, int drive_distance){
+  // translate inches to encoder target
+  int enc_target = CalculateEncoderTarget(drive_distance);
+  // swing turns first: to the left
+  if ((turn_type == TURN_TYPE_SWING) && (turn_dir == TURN_LEFT)){
+    Motor_Left.setSpeed(0);
+    Motor_Right.setSpeed(SetSpeed(encoder[RIGHT], enc_target, target_speed));
+    Motor_Left.run(RELEASE);
+    Motor_Right.run(FORWARD);
+  }
+  // swing turns first: to the right
+  if ((turn_type == TURN_TYPE_SWING) && (turn_dir == TURN_RIGHT)){
+    Motor_Left.setSpeed(SetSpeed(encoder[LEFT], enc_target, target_speed));
+    Motor_Right.setSpeed(0);
+    Motor_Left.run(FORWARD);
+    Motor_Right.run(RELEASE);
+  }
+  // point turns (i.e. Rotation in place): to the left...
+  if ((turn_type == TURN_TYPE_POINT) && (turn_dir == TURN_LEFT)){
+    Motor_Left.setSpeed(SetSpeed(encoder[LEFT], enc_target, target_speed));
+    Motor_Right.setSpeed(SetSpeed(encoder[RIGHT], enc_target, target_speed));
+    Motor_Left.run(BACKWARD);
+    Motor_Right.run(FORWARD);
+  }
+  // point turns (i.e. Rotation in place): to the right...
+  if ((turn_type == TURN_TYPE_POINT) && (turn_dir == TURN_RIGHT)){
+    Motor_Left.setSpeed(SetSpeed(encoder[LEFT], enc_target, target_speed));
+    Motor_Right.setSpeed(SetSpeed(encoder[RIGHT], enc_target, target_speed));
+    Motor_Left.run(FORWARD);
+    Motor_Right.run(BACKWARD);
+  } 
 }
 
 int CalculateEncoderTarget(float distance_inches){
@@ -287,4 +226,16 @@ int CalculateEncoderTarget(float distance_inches){
   // divide by the PulsePerRotation (PPR)
   // add 1/2 to ensure truncation falls on the right number
   return (int)((distance_inches / (PI * WHEEL_DIAMETER/PULSE_PER_ROTATION))+ 0.5);
+}
+
+int SetDriveDirection(DriveDirection drive_direction, int encoder_current_value, int encoder_target_value){
+  if (encoder_current_value < encoder_target_value){
+    return FORWARD;
+  }
+  else if (encoder_current_value > encoder_target_value){
+    return BACKWARD;
+  }
+  else {
+    return RELEASE; // Shield does not implement "BRAKE"
+  }
 }
